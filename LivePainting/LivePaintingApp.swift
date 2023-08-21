8c3f546bc840edfd57a31cc4cd4bf78395c5047f
rouzbeh <rouzbehmsv1988@gmail.com>
2023-08-21 10:15:40 +0200

    Initial Commit

---
+++ LivePainting/LivePaintingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivePaintingApp.swift
+//  LivePainting
+//
+//  Created by rouzbeh on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LivePaintingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
